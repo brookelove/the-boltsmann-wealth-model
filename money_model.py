273b6dd732252d21811ad7d3fec7317a11f4878f
brookelove <sblove@bu.edu>
2023-09-09 19:00:03 -0400

    creating more the addition of the 100 times and distribution of wealth

--- money_model.py
+++ money_model.py
@@ -1,16 +1,16 @@
-import mesa
-
-# Data visualization tools.
-import seaborn as sns
-
-# Has multi-dimensional arrays and matrices. Has a large collection of
+# Seaborrn data visualization tools.
+# Numpy Has multi-dimensional arrays and matrices. Has a large collection of
 # mathematical functions to operate on these arrays.
-import numpy as np
+# Pandas data manipulation and analysis.
 
-# Data manipulation and analysis.
+import mesa
+import seaborn as sns
+import numpy as np
 import pandas as pd
 
-''' - agents are entities that act in the model 
+
+'''
+    - agents are entities that act in the model 
     - in this model are ind. that exchange money
     - A model is a visualized grid for all the agents
     - the scheduler controls the order in which agents are activated caused the agents to take their defined action
@@ -21,13 +21,18 @@ class MoneyAgent(mesa.Agent):
     '''An agent with fixed wealth initally.'''
 
     def __init__(self, unique_id, model):
-        # parameters to teh parent class
+        # parameters to the parent class
         super().__init__(unique_id, model)
 
         # create the agent variable and set inital value
         self.wealth = 1
 
     def step(self):
+        if self.wealth > 0:
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent is not None:
+                other_agent.wealth += 1
+                self.wealth -= 1
         # The agent's step will go here.
         # For demonstration purposes we will print the agent's unique_id
         print(
--- run.py
+++ run.py
@@ -1,6 +1,24 @@
 #!/Users/brookelove/opt/miniconda3/envs/moneymodel01/bin/python
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from money_model import MoneyModel
 
+
+all_wealth = []
 print("Money Model")
-starter_model = MoneyModel(10)
-starter_model.step()
+# Running the model 100 times with model executing 10 steps
+for j in range(100):
+    # Run the model
+    model = MoneyModel(10)
+    for i in range(10):
+        model.step()
+
+    for agent in model.schedule.agents:
+        all_wealth.append(agent.wealth)
+
+g = sns.histplot(all_wealth, discrete=True)
+g.set(title="Wealth distribution", xlabel="wealth", ylabel="Number of Agents")
+
+plt.show()
